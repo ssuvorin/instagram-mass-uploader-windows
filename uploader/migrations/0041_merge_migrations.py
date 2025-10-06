bdbfa3cc0ddd19357ed669061304eac62b0daaaf
Sergey Suvorin <svrn.sergey@gmail.com>
2025-10-07 00:35:35 +0400

    fix: Add merge migration to resolve conflicting migration branches
    - Created 0041_merge_migrations.py to merge two migration branches
    - Branch 1: ends with 0032_remove_dolphincookierobottask_account_and_more
    - Branch 2: ends with 0040_remove_period_fields (analytics system)
    - This allows Windows version to apply all migrations successfully

---
+++ uploader/migrations/0041_merge_migrations.py
@@ -0,0 +1,16 @@
+# Generated manually to merge migration branches
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('uploader', '0032_remove_dolphincookierobottask_account_and_more'),
+        ('uploader', '0040_remove_period_fields'),
+    ]
+
+    operations = [
+        # No operations needed, this is just a merge migration
+    ]
+
