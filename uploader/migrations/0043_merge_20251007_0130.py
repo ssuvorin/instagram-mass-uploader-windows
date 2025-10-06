93e74a4af9683ca5bbce8dd1ad0ebe4b03c776e0
Sergey Suvorin <svrn.sergey@gmail.com>
2025-10-07 01:44:48 +0400

    Add merge migration 0043 to resolve conflicts

---
+++ uploader/migrations/0043_merge_20251007_0130.py
@@ -0,0 +1,16 @@
+# Generated manually to merge migration branches
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('uploader', '0041_merge_migrations'),
+        ('uploader', '0042_make_created_at_editable'),
+    ]
+
+    operations = [
+        # No operations needed, this is just a merge migration
+    ]
+
